442e92beb2d1f843fdc9de7da470a84b1ff0f066
Turbotard <turbotard9@gmail.com>
2024-03-06 08:13:52 +0100

    Add helmet and morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import run from './server';
 import 'dotenv/config';
 import routerTest from './Routes/testRoute';
 import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 3003;
+
+app.use(helmet())
+app.use(morgan('tiny'))
 
 app.use(cors());
 app.use(express.json());
