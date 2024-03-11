657c4b84cde9c4260d923963aac2049e143219d4
Turbotard <turbotard9@gmail.com>
2024-03-11 08:42:20 +0100

    Add userTestMiddleWare to userTest controller the right method for using the back

--- src/Controller/testController.ts
+++ src/Controller/testController.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express'
+import {userTestMiddleWare} from '../middleware/testMiddleWare'
 
 export const getHello = (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hello World!' })
@@ -7,19 +8,5 @@ export const getHello = (req: Request, res: Response) => {
 const prisma = new PrismaClient();
 
 export const userTest = async (req: Request,res: Response) => {
-    const { name, email } = req.body;
-    try {
-        const user = await prisma.user.create({
-        data: {
-            name ,
-            email,
-        },
-    });
-    res.status(200).json({ message : 'User added successfully'});
-    
-
-    }catch (error) {
-        res.status(500).json({ error: 'Something went wrong' });
-    }
-    
+    await userTestMiddleWare(req,res)    
 }
---
+++ src/middleware/testMiddleWare.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express'
+const prisma = new PrismaClient();
+
+
+export const userTestMiddleWare = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+            },
+        });
+        res.status(200).json({ message: 'User added successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+}
