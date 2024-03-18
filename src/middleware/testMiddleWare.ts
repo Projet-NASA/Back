b2735252823fad7f7b51519b0d138c5f54966ddb
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-03-18 12:14:24 +0100

    feat: Refactor userTestMiddleWare to include firstName and password in user creation

--- src/middleware/testMiddleWare.ts
+++ src/middleware/testMiddleWare.ts
@@ -2,15 +2,16 @@ import { Request, Response } from "express";
 import prisma from "../prisma";
 
 export const userTestMiddleWare = async (req: Request, res: Response) => {
-  const { name, email } = req.body;
+  const { firstName, email, password } = req.body;
   try {
     const user = await prisma.user.create({
       data: {
-        name,
+        firstName,
         email,
+        password,
       },
     });
-    res.status(200).json({ message: "User added successfully" });
+    res.status(200).json({ message: "User added successfully", user });
   } catch (error) {
     res.status(500).json({ error: "Something went wrong" });
   }
