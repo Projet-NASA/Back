d34e6535ba646faf29d9b22978ea7230ddfe91b7
Turbotard <turbotard9@gmail.com>
2024-03-21 10:26:55 +0100

    Refactor userController.ts to use middleware functions

--- src/Controller/userController.ts
+++ src/Controller/userController.ts
@@ -1,120 +1,29 @@
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import prisma from "../prisma";
+import {
+  createUser as createUserFromMiddleware,
+  getUser as getUserFromMiddleware,
+  getUsers as getUsersFromMiddleware,
+  updateUser as updateUserFromMiddleware,
+  deleteUser as deleteUserFromMiddleware,
+} from "../middleware/userMiddleWare";
 
-const bcrypt = require("bcrypt");
 
 export const createUser = async (req: Request, res: Response) => {
-  const { firstName, lastName, email } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password: await bcrypt.hash("password", 10),
-      },
-    });
-    res.status(200).json({ message: "utilisateur ajouté avec succès", user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "une erreur est survenue" });
-  }
+  await createUserFromMiddleware(req, res);
 };
 
 export const getUsers = async (req: Request, res: Response) => {
-  try {
-    const retrievedUsers = await prisma.user.findMany();
-    res.status(200).json(retrievedUsers);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "une erreur est survenue" });
-  }
+  await getUsersFromMiddleware(req, res);
 };
 
 export const getUser = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const retrievedUser = await prisma.user.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    res.status(200).json(retrievedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "une erreur est survenue" });
-  }
+  await getUserFromMiddleware(req, res);
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { firstName, lastName, email } = req.body;
-  try {
-    const updatedUser = await prisma.user.update({
-      where: {
-        id: id,
-      },
-      data: {
-        firstName,
-        lastName,
-        email,
-      },
-    });
-    res
-      .status(200)
-      .json({ message: "utilisateur mis à jour avec succès", updatedUser });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "une erreur est survenue" });
-  }
+  await updateUserFromMiddleware(req, res);
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    await prisma.user.delete({
-      where: {
-        id: id,
-      },
-    });
-    res.status(200).json({ message: "utilisateur supprimé avec succès" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "une erreur est survenue" });
-  }
-};
-
-export const loginUser = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (!user) {
-      return res.status(400).json({ error: "utilisateur non trouvé" });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(400).json({ error: "mot de passe incorrect" });
-    }
-
-    if (!process.env.JWT_SECRET) {
-      throw new Error(" la clé JWT_SECRET n'est pas définie");
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    res.status(200).json({ message: "connexion réussie", user, token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "une erreur est survenue" });
-  }
+  await deleteUserFromMiddleware(req, res);
 };
