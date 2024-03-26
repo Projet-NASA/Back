7857d085eb24aa79fd331c317d4cd8f7141ba930
Turbotard <turbotard9@gmail.com>
2024-03-26 12:38:14 +0100

    Refactor middleware functions for creating, getting, updating, and deleting posts and likes

--- src/middleware/likeMiddleWare.ts
+++ src/middleware/likeMiddleWare.ts
@@ -3,18 +3,25 @@ import jwt from "jsonwebtoken";
 import prisma from "../prisma";
 
 export const createLike = async (req: Request, res: Response) => {
-  const { userId, postId } = req.body;
-  try {
-    const like = await prisma.like.create({
-      data: {
-        userId,
-        postId,
-      },
-    });
-    res.status(200).json({ message: "Like added successfully", like });
-  } catch (error) {
-    res.status(500).json({ error: "Something went wrong" });
-  }
+    const { postId, userId } = req.body;
+    try {
+        await prisma.like.create({
+            data: {
+                postId,
+                userId,
+            },
+        });
+        const likesCount = await prisma.like.count({
+            where: { postId },
+        });
+        await prisma.post.update({
+            where: { id: postId },
+            data: { like: likesCount },
+        });
+        res.status(200).json({ message: "Like added successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong" });
+    }
 };
 
 export const getLikes = async (req: Request, res: Response) => {
