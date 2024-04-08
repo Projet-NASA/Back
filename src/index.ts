18fdf87409db278c95fdfb3415995b6f24dc5a0a
Turbotard <turbotard9@gmail.com>
2024-04-08 15:01:35 +0200

    Remove unnecessary allowedHeaders option in corsOptions

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,6 @@ const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'], 
 };
 
 app.use(cors(corsOptions));
