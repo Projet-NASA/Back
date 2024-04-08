b8ff0fddbbe23b64903747e7aae0e3d2eb28f158
Turbotard <turbotard9@gmail.com>
2024-04-08 14:06:06 +0200

    Add loginUser and newGetUserSessions to userController and update userRoute

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'], 
 };
 
 app.use(cors(corsOptions));
